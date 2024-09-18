af7ea6db53b98e296ffe1a4aea20425d28860b46
comborush <comborush.init@proton.me>
2024-09-18 04:09:06 +0300

    making the best page

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,12 @@
 <template>
-    <main class="w-full h-full">
-        <div id="Header" class="flex flex-col w-full h-screen p-3">
-            <h1 id="trans-text-1" class="absolute top-0 left-0 text-[13rem] font-bold uppercase">Software</h1>
-            <div class="flex flex-col w-full h-full justify-center items-center">
-                <h1 class="text-8xl font-bold">DrunkTaric</h1>
-                <button class="text-3xl" @click="playSound">Click me</button>
-            </div>
-            <h1 id="trans-text-2" class="absolute bottom-0 right-0 text-[13rem] font-bold uppercase">Engineer</h1>
-        </div>
-    </main>
+    <Header></Header>
+    <div id="Technology Bar" class="flex w-full max-h-28 border-y-4 space-x-10 p-5 pt-2 pb-2">
+        <img src="/favicon.ico" class="size-10" />
+        <img src="/favicon.ico" class="size-10" />
+        <img src="/favicon.ico" class="size-10" />
+        <img src="/favicon.ico" class="size-10" />
+        <img src="/favicon.ico" class="size-10" />
+    </div>
+    <div id="About" class="flex flex-col w-full h-screen p-3 overflow-x-hidden">
+    </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            audio: null
-        }
-    },
-    methods: {
-        playSound() {
-            this.audio = new Audio('/click.mp3');
-            this.audio.play();
-        }
-    }
-}
-</script>
