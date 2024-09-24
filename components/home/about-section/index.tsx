f35117e79cbe7a88c6167340b61d0717880d2206
comborush <comborush.init@proton.me>
2024-09-24 21:29:43 +0300

    trying things

--- components/home/about-section/index.tsx
+++ components/home/about-section/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function About({ cursor }: {cursor: Omit<CursorExport, "RenderedComponent"> }) {
     return (
-        <section className="relative w-full h-screen bg-foreground" onMouseEnter={() => {cursor.changeColor("transparent")}} onMouseLeave={() => {cursor.changeColor("white")}}>
+        <section className="relative w-full h-[150vh] bg-foreground" onMouseEnter={() => {cursor.changeColor("transparent")}} onMouseLeave={() => {cursor.changeColor("white")}}>
             <div className="custom-shape-divider-top-1727095274">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
@@ -17,8 +17,13 @@ export default function About({ cursor }: {cursor: Omit<CursorExport, "RenderedC
                     <h1 className="text-black text-5xl font-bold line-through">Tying to find what is possible using code.</h1>
                     <h1 className="text-black text-5xl font-bold">Tying to Push the limits and beyond.</h1>
                 </div>
-                <div className="">
-                    <Image />
+                <div className="flex pt-20">
+                    <div className="w-[50%]">
+                        <h1>Hello</h1>
+                    </div>
+                    <div className="w-[50%]">
+                        <Image width={500} height={1000} alt="a photo of myself" src={"/photos/me.png"} className="overflow-hidden rounded-2xl w-[500px] h-[800px] bg-cover object-top object-cover ml-auto mr-auto" />
+                    </div>
                 </div>
             </div>
         </section>
