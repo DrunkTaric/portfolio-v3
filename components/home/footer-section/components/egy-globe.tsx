3e331768c62a29ec609453adcaf27b63b72bfea5
comborush <comborush.init@proton.me>
2024-09-30 10:49:15 +0300

    my 3d component

---
+++ components/home/footer-section/components/egy-globe.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useEffect, useRef } from 'react';
+import Globe from 'react-globe.gl';
+
+export default function EgyGlobe() {
+
+    const globeEl = useRef();
+
+    const pointsData = [
+        {
+          lat: 26.8206, // Approximate latitude of Egypt
+          lng: 30.8025, // Approximate longitude of Egypt
+          label: "Egypt", // Label to display
+          population: 102334000, // Optional: additional info
+        }
+    ];
+
+    useEffect(() => {
+        const globe = globeEl.current;
+        globe.controls().autoRotate = false; // Disable auto-rotation if you don't want it
+        globe.controls().enableZoom = false; // Enable zoom if you want it
+        globe.pointOfView({ lat: 26.0, lng: 30.0, altitude: 2 }, 0);
+        
+    }, [])
+
+    return (
+        <Globe
+            ref={globeEl}
+            width={700}
+            height={700}
+            pointsData={pointsData}
+            pointRadius={0.9}
+            backgroundColor='rgba(0,0,0,0)'
+            enablePointerInteraction={true}
+            pointColor={() => 'rgba(20, 168, 3, 1)'}
+            globeImageUrl={"https://unpkg.com/three-globe@2.31.3/example/img/earth-dark.jpg"}
+            bumpImageUrl={"https://unpkg.com/three-globe@2.31.3/example/img/earth-topology.png"}
+        ></Globe>
+    )
+}
