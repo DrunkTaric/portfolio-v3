fa55c5367536f7c610400cfec21cbabc468a07ae
comborush <comborush.init@proton.me>
2024-09-30 10:49:05 +0300

    about sectoin :)

--- components/home/about-section/index.tsx
+++ components/home/about-section/index.tsx
@@ -1,24 +1,36 @@
 import { CursorExport } from "@/hooks/useCursor"
-import "./style.css"
+import { Caveat } from "next/font/google"
 import Image from "next/image"
+import clsx from "clsx"
+import "./style.css"
+
+
+const caveat = Caveat({
+    subsets: ["cyrillic"],
+    weight: "600",
+    display: "swap"
+})
 
 export default function About({ cursor }: {cursor: Omit<CursorExport, "RenderedComponent"> }) {
     return (
-        <section className="relative w-full h-[150vh] bg-background" onMouseEnter={() => {cursor.changeColor("transparent")}} onMouseLeave={() => {cursor.changeColor("white")}}>
-            <div className="flex flex-col p-10">
-                <h1 className="text-7xl ml-auto mr-auto font-bold uppercase">About Me</h1>
-                <div className="pt-20 space-y-10">
-                    <h1 className="text-5xl font-bold line-through">I am just a programmer, Trying to figure things.</h1>
-                    <h1 className="text-5xl font-bold line-through">Tying to find what is possible using code.</h1>
-                    <h1 className="text-5xl font-bold">Tying to Push the limits and beyond.</h1>
+        <section className="flex flex-col p-20 w-full h-screen bg-background space-y-10">
+            <h1 className={"text-8xl font-bold"}>About</h1>
+            <div className="flex">
+                <div className="w-[60%] space-y-10 mt-auto mb-auto">
+                    <h1 className={clsx("text-5xl", caveat.className)}>
+                        Hi, I’m Mohamed Ismail, a full-stack developer and systems architect.
+                    </h1>
+                    <h1 className={clsx("text-5xl", caveat.className)}>
+                        I build things from scratch to understand how they work and to unlock new creative 
+                        possibilities.
+                    </h1>
+                    <h1 className={clsx("text-5xl", caveat.className)}>
+                        I’m passionate about solving complex problems and constantly pushing the limits 
+                        of what’s possible in software development.
+                    </h1>
                 </div>
-                <div className="flex pt-20">
-                    <div className="w-[50%]">
-                        <h1>Hello</h1>
-                    </div>
-                    <div className="w-[50%]">
-                        <Image width={500} height={1000} alt="a photo of myself" src={"/photos/me.png"} className="overflow-hidden rounded-2xl w-[500px] h-[800px] bg-cover object-top object-cover ml-auto mr-auto" />
-                    </div>
+                <div className="w-[40%]">
+                    <Image width={400} height={400} alt="a photo of myself" src={"/photos/hi.png"} className="overflow-hidden rounded-2xl bg-cover object-top object-cover ml-auto mr-auto" />
                 </div>
             </div>
         </section>
